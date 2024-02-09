0b8f905cf4d5e764c7705444927bfe3801371c86
David Guerreiro <me@davidguerreiro.com>
2024-02-09 18:13:50 +0000

    feat: create level 2-1
    feat: add new background and platforms prefabs
    
    feat: add lave prefabs
    
    feat: change health and key UI background color

---
+++ Graphics/Images/Background/CaveBackground/CaveBackgroundColorAnimation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveBackgroundColorAnimation : MonoBehaviour
+{
+    // Create script to animate background.
+    // Create jumping lava hazard.
+    // Create falling spikes.
+    // Create bat enemy.
+    // Create slime enemy.
+    // Create wheel enemy.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
