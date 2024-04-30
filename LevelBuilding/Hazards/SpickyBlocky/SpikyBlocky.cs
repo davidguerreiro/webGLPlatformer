002686b456aa50c50ff56613e8b771930d8f3007
David Guerreiro <me@davidguerreiro.com>
2024-04-30 17:11:51 +0100

    feat: complete spicky block hazard

---
+++ LevelBuilding/Hazards/SpickyBlocky/SpikyBlocky.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikyBlocky : MonoBehaviour
+{
+    [Header("Settings")]
+    public float speed;
+    public float waitBetweenMovements;
+
+    [Header("Components")]
+    public GameManager gameManager;
+    public Transform[] movingPoints;
+
+    private AudioComponent _audio;
+    private Coroutine _movingRoutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.inGamePlay)
+        {
+            if (_movingRoutine == null)
+            {
+                _movingRoutine = StartCoroutine(MovePointToPoint());
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Move hazard to each point in endless loop.
+    /// </summary>
+    /// <returns>IEnumerator</returns>
+    public IEnumerator MovePointToPoint()
+    {
+        for (int i = 0; i < movingPoints.Length; i++)
+        {
+            int nextIndex = i + 1;
+            Transform target = (nextIndex == movingPoints.Length) ? movingPoints[0] : movingPoints[nextIndex];
+
+            while (Vector2.Distance(transform.position, target.position) > 0.01f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                yield return new WaitForFixedUpdate();
+            }
+
+            _audio.PlaySound();
+
+            transform.position = target.position;
+            yield return new WaitForSeconds(waitBetweenMovements);
+        }
+
+        _movingRoutine = null;
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init()
+    {
+        _audio = GetComponent<AudioComponent>();
+    }
+}
